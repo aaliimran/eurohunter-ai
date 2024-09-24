1e49e3c017f6f5f29e9da64be66839023200ecb7
aaliimran <alikubanov@gmail.com>
2024-09-24 20:25:31 +0600

    disable google auth button

--- src/components/buttons/google-auth-button.tsx
+++ src/components/buttons/google-auth-button.tsx
@@ -1,24 +1,57 @@
+// "use client";
+// import React from "react";
+// import Image from "next/image";
+// import { redirect } from "next/navigation";
+// import { signIn, useSession } from "next-auth/react";
+
+// const GoogleAuthButton = () => {
+//   const { status } = useSession();
+
+//   if (status === "loading") {
+//     return <p>Loading ...</p>;
+//   }
+//   if (status === "authenticated") {
+//     redirect("/pages/auth/start/");
+//   }
+
+//   return (
+//     <div className="w-[107%] flex pt-[38px]">
+//       <button
+//         className="w-full flex p-4 px-5 justify-center items-center gap-2 rounded-lg border border-gray-300 relative "
+//         onClick={() => signIn("google")}
+//       >
+//         <Image
+//           className="absolute left-4"
+//           src="/icons/auth-icons/google.svg"
+//           alt="Google Icon"
+//         />
+//         <span className="text-black font-sans text-lg font-semibold leading-[128%] tracking-[0.064px]">
+//           Continue with Google
+//         </span>
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default GoogleAuthButton;
+
 "use client";
 import React from "react";
 import Image from "next/image";
-import { redirect } from "next/navigation";
-import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const GoogleAuthButton = () => {
-  const { status } = useSession();
+  const router = useRouter();
 
-  if (status === "loading") {
-    return <p>Loading ...</p>;
-  }
-  if (status === "authenticated") {
-    redirect("/pages/auth/start/");
-  }
+  const handleRedirect = () => {
+    router.push("/pages/auth/start/");
+  };
 
   return (
     <div className="w-[107%] flex pt-[38px]">
       <button
         className="w-full flex p-4 px-5 justify-center items-center gap-2 rounded-lg border border-gray-300 relative "
-        onClick={() => signIn("google")}
+        onClick={handleRedirect}
       >
         <Image
           className="absolute left-4"
@@ -26,7 +59,7 @@ const GoogleAuthButton = () => {
           alt="Google Icon"
         />
         <span className="text-black font-sans text-lg font-semibold leading-[128%] tracking-[0.064px]">
-          Continue with Google
+          Continue without Google
         </span>
       </button>
     </div>
